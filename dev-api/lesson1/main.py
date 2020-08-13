c149790314fedf5df4e29fcfb6b107841d57ecb9
nsvistov <swistov@gmail.com>
2020-08-13 13:54:24 +0300

    - dev-api lesson1

---
+++ dev-api/lesson1/main.py
@@ -0,0 +1,20 @@
+import requests
+from requests.exceptions import HTTPError
+
+
+cities = ['Лондон', 'Череповец', 'Шереметьево']
+payload = {"lang": "ru"}
+
+
+def print_weather(site_url):
+    response = requests.get(site_url, params=payload)
+    try:
+        response.raise_for_status()
+        print(response.text)
+    except HTTPError:
+        print('Прогноз недоступен')
+
+
+for city in cities:
+    url = f'http://wttr.dvmn.org/{city}?nTqm'
+    print_weather(url)
