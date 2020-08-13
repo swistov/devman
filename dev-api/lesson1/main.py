6e1c5a5a3e70df7ebeb511685002626649bd2e54
nsvistov <swistov@gmail.com>
2020-08-13 18:34:36 +0300

    - dev-api Параметры запроса вынесены в словарь

--- dev-api/lesson1/main.py
+++ dev-api/lesson1/main.py
@@ -3,19 +3,20 @@ from requests.exceptions import HTTPError
 
 
 CITIES = ['Лондон', 'Череповец', 'Шереметьево']
-PAYLOAD = {"lang": "ru"}
+PAYLOAD = {'lang': 'ru', 'nTqm': ''}
 
 
 def print_weather(site_url):
     response = requests.get(site_url, params=PAYLOAD)
     try:
         response.raise_for_status()
-        print(response.text)
+        print(response.url)
+        # print(response.text)
     except HTTPError:
         print('Прогноз недоступен')
 
 
 if __name__ == '__main__':
     for city in CITIES:
-        url = f'http://wttr.dvmn.org/{city}?nTqm'
+        url = f'http://wttr.dvmn.org/{city}'
         print_weather(url)
